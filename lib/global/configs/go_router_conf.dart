c6451b2d5b7caf6c4a1759a9f812e25662cb326a
Ahmad Faisal <ahmad.faisalewy@gmail.com>
2023-09-17 14:04:07 +0700

    feat: added go_router to handle dynamic links

---
+++ lib/global/configs/go_router_conf.dart
@@ -0,0 +1,44 @@
+import 'package:didpoolfit/modules/auth/login/login_page.dart';
+import 'package:didpoolfit/modules/auth/register/choose_program_page.dart';
+import 'package:didpoolfit/modules/auth/register/complete_profile_page.dart';
+import 'package:didpoolfit/modules/auth/register/register_page.dart';
+import 'package:didpoolfit/modules/welcome/welcome_page.dart';
+import 'package:go_router/go_router.dart';
+
+final routers = GoRouter(
+  initialLocation: '/welcome',
+  routes: [
+    GoRoute(
+      name: 'welcome',
+      path: '/welcome',
+      builder: (context, state) => const WelcomePage(),
+    ),
+    GoRoute(
+      name: 'register',
+      path: '/register',
+      builder: (context, state) => const RegisterPage(),
+    ),
+    GoRoute(
+      name: 'login',
+      path: '/login',
+      builder: (context, state) => const LoginPage(),
+    ),
+    GoRoute(
+      name: 'complete-profile',
+      path: '/register/complete-profile',
+      builder: (context, state) {
+        final Map<String, String> userData = state.extra as Map<String, String>;
+        return CompleteProfilePage(userData: userData);
+      },
+    ),
+    GoRoute(
+      name: 'choose-program',
+      path: '/register/complete-profile/choose-program',
+      builder: (context, state) {
+        final Map<String, dynamic> userData =
+            state.extra as Map<String, dynamic>;
+        return ChooseProgramPage(userData: userData);
+      },
+    ),
+  ],
+);
